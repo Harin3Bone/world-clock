4f011262295b355282f6f7ddcd1a4868a17a6628
Harin3Bone <harin.thananam@gmail.com>
2024-03-31 14:37:53 +0700

    Initial commit

---
+++ src/main/java/com/worldclock/harin/WorldClockBeApplication.java
@@ -0,0 +1,13 @@
+package com.worldclock.harin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldClockBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldClockBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldclock/harin/WorldClockBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldclock.harin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldClockBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
